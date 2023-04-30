a97d268fe353f1e6d1da4ec3feea93820f0d50ef
Deimante <deima.vait19@gmail.com>
2023-04-30 14:47:02 +0300

    task 1 js done

--- Task 1/script.js
+++ Task 1/script.js
@@ -8,3 +8,28 @@ pamatyti jo pateikto svorio kovertavimą į:
 Pastaba: rezultatas turi būti matomas pateikus formą ir atvaizduojamas
 <div id="output"></div> viduje. Gautus atsakymus stilizuokite naudojant CSS;
 ------------------------------------------------------------------- */
+
+const form = document.querySelector('form');
+const search = document.querySelector('#search');
+const output = document.querySelector('#output');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const kg = parseFloat(search.value);
+
+  if (!isNaN(kg)) {
+    const lb = (kg * 2.2046).toFixed(2);
+    const g = (kg / 0.001).toFixed(2);
+    const oz = (kg * 35.274).toFixed(2);
+
+    output.innerHTML = `<p>${kg} kg is equal to:</p>
+                        <ul>
+                          <li>${lb} lb</li>
+                          <li>${g} g</li>
+                          <li>${oz} oz</li>
+                        </ul>`;
+  } else {
+    output.innerHTML =
+      '<p>Please enter a valid number for weight in kilograms.</p>';
+  }
+});
