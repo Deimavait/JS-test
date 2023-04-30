cc9b9a2ef14adafc77144a402333e900751dc80e
Deimante <deima.vait19@gmail.com>
2023-04-30 15:01:41 +0300

    task 4 completed

--- Task 4/script.js
+++ Task 4/script.js
@@ -9,3 +9,34 @@ būti stilizuota su CSS ir būti responsive;
 -------------------------------------------------------------------------- */
 
 const ENDPOINT = 'cars.json';
+
+const createCards = (json) => {
+  const outputContainer = document.getElementById('output');
+
+  json.forEach((car) => {
+    const cardElement = document.createElement('div');
+    cardElement.classList.add('card');
+
+    const brandElement = document.createElement('h1');
+    brandElement.textContent = car.brand;
+    cardElement.appendChild(brandElement);
+
+    const modelsElement = document.createElement('ul');
+    car.models.forEach((model) => {
+      const listItemElement = document.createElement('li');
+      listItemElement.textContent = model;
+      modelsElement.appendChild(listItemElement);
+    });
+    cardElement.appendChild(modelsElement);
+
+    outputContainer.appendChild(cardElement);
+  });
+};
+
+async function getCarsData() {
+  const response = await fetch(ENDPOINT);
+  const json = await response.json();
+  createCards(json);
+}
+
+getCarsData();
