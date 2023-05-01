27b348b65da6f4d912175426948905ca7b652d41
Deimante <deima.vait19@gmail.com>
2023-05-01 22:57:55 +0300

    task 8 completed

--- Task 8/script.js
+++ Task 8/script.js
@@ -5,3 +5,24 @@ subtraction(a, b) - priima du skaičius ir grąžina jų skirtumą.
 multiplication(a, b) - priima du skaičius ir grąžina jų daugybos rezultatą;
 division(a, b) - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+
+function Calculator() {
+  return {
+    sum: function (a, b) {
+      return a + b;
+    },
+    subtraction: function (a, b) {
+      return a - b;
+    },
+    multiplication: function (a, b) {
+      return a * b;
+    },
+    division: function (a, b) {
+      return a / b;
+    },
+  };
+}
+console.log(Calculator().sum(6, 3));
+console.log(Calculator().subtraction(6, 3));
+console.log(Calculator().multiplication(6, 3));
+console.log(Calculator().division(6, 3));
